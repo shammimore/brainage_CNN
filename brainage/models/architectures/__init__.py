3cb1521a3425f1fc937daa3b02733999772241b1
Shammi More <s.more@fz-juelich.de>
2023-08-02 15:10:54 +0200

    update init for new architecture

--- brainage/models/architectures/__init__.py
+++ brainage/models/architectures/__init__.py
@@ -1,6 +1,7 @@
 from ._resnet3d import (ResNet, resnet18, resnet34, resnet50, resnet101,
                        resnet152)
 from ._sfcn import SFCN
+from ._rank_sfcn import RankSFCN
 
 __all__ = ['ResNet',
            'resnet18',
@@ -8,4 +9,5 @@ __all__ = ['ResNet',
            'resnet50',
            'resnet101',
            'resnet152',
-           'SFCN']
+           'SFCN',
+           'RankSFCN']
