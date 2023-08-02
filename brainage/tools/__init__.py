2fe4bfefa268e84abbb14426ab3e1b8366438ae6
Shammi More <s.more@fz-juelich.de>
2023-08-02 10:13:42 +0200

    updated init

--- brainage/tools/__init__.py
+++ brainage/tools/__init__.py
@@ -1,8 +1,10 @@
 from ._additional_functions import (crop_center, get_batch, get_bin_centers,
                                     num2vect, random_seed)
+from ._checking_functions import check_brain_age_predictor
 
 __all__ = ['crop_center',
            'get_batch',
            'get_bin_centers',
            'num2vect',
-           'random_seed']
+           'random_seed',
+           'check_brain_age_predictor']
